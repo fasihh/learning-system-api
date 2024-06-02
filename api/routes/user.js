bcf96520dedd96bcd765438ca1f41478b4719260
fasihh <fhk.tcs@gmail.com>
2024-06-02 23:03:45 +0500

    using jwtAuth middleware

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,8 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const UserController = require('../controllers/user');
+const auth = require('../auth/jwtAuth');
 
-router.get('/', UserController.getAllUsers);
+router.get('/', auth, UserController.getAllUsers);
 
 router.post('/signin', UserController.userSignIn);
 router.post('/signup', UserController.userSignUp);
