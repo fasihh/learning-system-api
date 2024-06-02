8caba27a4a22b0d4cbd47504cc6f5a4dd6e32414
fasihh <fhk.tcs@gmail.com>
2024-06-02 22:39:32 +0500

    Using new dependencies
    - Changed getAllUsers so it uses the new asyncHandler
    - Completed userSignIn. Also used the jwt tokens and hashing
    - Added hashing in userSignUp

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,43 +1,60 @@
 const { sequelize, User } = require('../../db');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const ash = require('../utils/asyncHandler');
 
-module.exports.getAllUsers = (req, res, next) => {
-    sequelize.sync()
-    .then(async () => {
-        try {
-            const users = await User.findAll();
-            console.log(users);
-            return res.status(200).json({
-                message: 'All users retrieved',
-                count: users.length,
-                users: users.map()
-            });
-        } catch(error) {
-            console.error(error);
-            return res.status(400).json({ message: 'Could not get all users', error: error });
-        }
-    })
-    .catch(error => res.status(500).json({ message: 'An error has occured', error: error }));
-}
+module.exports.getAllUsers = ash(async (req, res, next) => {
+    await sequelize.sync();
+    const users = await User.findAll();
 
-module.exports.userSignIn = (req, res, next) => {
     return res.status(200).json({
-        message: 'User signin successful'
-    })
-}
+        message: 'All users retrieved',
+        count: users.length,
+        users: users.map(user => ({
+            id: user.id,
+            username: user.username,
+            createdAt: user.createdAt
+        }))
+    });
+});
 
-module.exports.userSignUp = (req, res, next) => {
-    sequelize.sync()
-    .then(async () => {
-        try {
-            await User.create({
-                username: req.body.username,
-                password: req.body.password,
-            });
-            return res.status(201).json({ message: 'User created successfully' });
-        } catch(error) {
-            console.error(error);
-            return res.status(400).json({ message: 'User could not be created', error: error });
+module.exports.userSignIn = ash(async (req, res, next) => {
+    await sequelize.sync();
+    const authFailed = () => res.status(401).json({ message: 'User auth failed' });
+
+    const user = await User.findOne({ where: { username: req.body.username }});
+    if (!user) return authFailed();
+
+    const status = await bcrypt.compare(req.body.password, user.password);
+    if (!status) return authFailed();
+
+    const token = jwt.sign(
+        {
+            username: user.username,
+            userId: user.id
+        },
+        process.env.JWT_KEY,
+        {
+            expiresIn: '1h'
         }
-    })
-    .catch(error => res.status(500).json({ message: 'An error has occured', error: error }));
-}
+    );
+
+    return res.status(200).json({
+        message: 'User signed in successfully',
+        token: `Bearer ${token}`
+    });
+});
+
+module.exports.userSignUp = ash(async (req, res, next) => {
+    await sequelize.sync();
+
+    const user = await User.findOne({ where: { username: req.body.username }});
+    if (user) return res.status(409).json({ message: 'User with this username already exists!'});
+
+    await User.create({
+        username: req.body.username,
+        password: await bcrypt.hash(req.body.password, 10),
+    });
+    
+    return res.status(201).json({ message: 'User created successfully' });
+});
