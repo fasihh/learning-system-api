489fb3ae1231c4ccfab1ab7e1bec2195da159d40
fasihh <fhk.tcs@gmail.com>
2024-06-02 22:37:54 +0500

    changed signin route to post instead of get (dunno why it was get)

--- api/routes/user.js
+++ api/routes/user.js
@@ -5,8 +5,8 @@ const router = express.Router();
 const UserController = require('../controllers/user');
 
 router.get('/', UserController.getAllUsers);
-router.get('/signin', UserController.userSignIn);
 
+router.post('/signin', UserController.userSignIn);
 router.post('/signup', UserController.userSignUp);
 
 module.exports = router;
