4ad5fe12064b6ee6d8918247f1f091f565c4a24b
fasihh <fhk.tcs@gmail.com>
2024-06-03 10:10:21 +0500

    ocd...

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const http = require('http')
-const { app } = require('./app')
+const http = require('http');
+const { app } = require('./app');
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-server.listen(port)
+server.listen(port);
 
-console.log(`Server is running on: http://localhost:${port}`)
+console.log(`Server is running on: http://localhost:${port}`);
